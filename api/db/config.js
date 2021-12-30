1f3e580303010cdc486c3592b8732a883a77dce6
iyan <iyan.cupliz@gmail.com>
2021-12-30 11:00:01 +0700

    move db to mysql2

--- api/db/config.js
+++ api/db/config.js
@@ -1,14 +1,26 @@
+require('dotenv').config()
 const path = require('path')
 module.exports = {
-  client: 'sqlite3',
-  connection: {
-    filename: path.join(__dirname, './mydb.sqlite3'),
-  },
+  client: 'mysql2',
+  connection: process.env.DB_URL,
+  // client: 'sqlite3',
+  // connection: {
+  //   filename: path.join(__dirname, './mydb.sqlite3'),
+  // },
   migrations: {
     directory: path.join(__dirname, './migrations'),
   },
   seeds: {
     directory: path.join(__dirname, './seeds'),
+  },
+  pool: {
+    min: 2,
+    max: 10,
+    afterCreate: function (conn, cb) {
+      conn.query('SET sql_mode="";', function (err) {
+        cb(err, conn)
+      })
+    }
   },
   useNullAsDefault: true
 }
